5a56b467efe54707d61b9137f457c29e739e2b70
OswaldCc <sam.kabati@gmail.com>
2023-01-28 11:39:38 +0300

    small refactor

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# kenzzy
+# Kenzzy
+
+Kenzzy is a chatbot that uses OpenAI's GPT-3 to respond to user's questions and provide them with relevant information. The bot was developed using Python and JavaScript, and it makes API calls to the OpenAI API to generate responses. 
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+
+You will need to have Python installed on your machine. You will also need to sign up for an API key from OpenAI.
+
+### Installing
+
+1. Clone the repository git clone https://github.com/your-username/kenzzy.git
+
+2. Install the Python dependencies
+
+3. Add your OpenAI API key to the `.env` file
+
+4. Run the app
+
+
+## Usage
+
+The app will start a local server on your machine. You can access the chatbot by navigating to `http://localhost:8000` in your web browser.
+
+## Built With
+
+* [Python](https://www.python.org/) - The programming language used for the backend
+* [JavaScript](https://www.javascript.com/) - Used for the frontend
+* [OpenAI API](https://openai.com/) - Used for the natural language processing and generation
+
+## Authors
+
+* **Samuel Kabati** - [Your GitHub](https://github.com/your-username)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
--- kenzzy/static/js/localStorage.js
+++ kenzzy/static/js/localStorage.js
@@ -1,59 +1,60 @@
-
 function saveChat(chatDiv){
-let chatDivs= JSON.parse(sessionStorage.getItem('chatsDiv')) || [];
-chatDivs.push(chatDiv)
-sessionStorage.setItem('chatsDiv',JSON.stringify(chatDivs));
-
- }
+  // Saves the chat div element to session storage
+  let chatDivs= JSON.parse(sessionStorage.getItem('chatsDiv')) || [];
+  chatDivs.push(chatDiv)
+  sessionStorage.setItem('chatsDiv',JSON.stringify(chatDivs));
+}
 
 function getChats(){
+  // Gets the chat div elements from session storage
   let chatDivs = JSON.parse(sessionStorage.getItem('chatsDiv')) || [];
   return chatDivs;
 }
 
-  
- function appendChats(){
+function appendChats(){
+  // Appends the chat div elements to the 'conv' parent div
   const chats=getChats();
   const parentDiv=document.getElementById('conv');
   chats.forEach(chat=>{
-    const div=document.createElement('div');
-    div.innerHTML= `${chat}`
-    parentDiv.appendChild(div);
-   
+      const div=document.createElement('div');
+      div.innerHTML= `${chat}`
+      parentDiv.appendChild(div);
   })
   if (chats.length>=2){
-    const imageDiv=document.getElementById('kenzzyImg')
-    const messageDiv=document.getElementById('message')
-    const quotesDiv=document.getElementById('quote')
-    imageDiv.remove()
-    messageDiv.remove()
-    quotesDiv.remove()
+      // removes the image, message and quotes divs
+      const imageDiv=document.getElementById('kenzzyImg')
+      const messageDiv=document.getElementById('message')
+      const quotesDiv=document.getElementById('quote')
+      imageDiv.remove()
+      messageDiv.remove()
+      quotesDiv.remove()
   }
- }
+}
 
 function saveChatHistory(chat){
+  // Saves the chat history to session storage
   let chats = JSON.parse(sessionStorage.getItem('chats')) || [];
   chats.push(chat)
   sessionStorage.setItem('chats',JSON.stringify(chats));
-   }
+}
 
 function getChatHistory(){
-let chats = JSON.parse(sessionStorage.getItem('chats')) || [];
-return chats;
-}
-  
-function addChatHistoryToForm(){
-    const chats=getChatHistory();
-    const parentDiv=document.getElementById('saved_chats');
-    chats.forEach(chat=>{
-      parentDiv.value=chat;
-    })
+  // Gets the chat history from session storage
+  let chats = JSON.parse(sessionStorage.getItem('chats')) || [];
+  return chats;
 }
 
+function addChatHistoryToForm(){
+  // Adds the chat history to the 'saved_chats' form field
+  const chats=getChatHistory();
+  const parentDiv=document.getElementById('saved_chats');
+  chats.forEach(chat=>{
+      parentDiv.value=chat;
+  })
+}
 
 function displayRandomQuote() {
   // Create an array of quotes
-  
   let quotes = [
     "Be the change you wish to see in the world. - Mahatma Gandhi",
     "The only limit to our realization of tomorrow will be our doubts of today. - Franklin D. Roosevelt",
@@ -69,17 +70,43 @@ function displayRandomQuote() {
     "Believe you can and you're halfway there. - Theodore Roosevelt",
     "The only way to do great work is to love what you do. If you haven’t found it yet, keep looking. - Steve Jobs"
   ];
-
   // Generate a random number between 0 and the length of the quotes array
   let randomNumber = Math.floor(Math.random() * quotes.length);
-
   // Use the random number to select a quote from the array
   let randomQuote = quotes[randomNumber];
-
   // Display the quote
   const parentDiv=document.getElementById('quote');
   const div=document.createElement('div');
   div.className = 'quotes'
   div.innerHTML= `${randomQuote}`
   parentDiv.appendChild(div);
-};
+}
+
+function textAreaAdjust(element) {
+  // Resizes a text area to fit the text
+  element.style.height = "1px";
+  element.style.height = (10+element.scrollHeight)+"px";
+}
+
+function saveQuote(){
+  // Saving the current displayed quote to session storage
+  let quote=document.getElementById('quote').innerText
+  let quotes = JSON.parse(sessionStorage.getItem('quotes')) || [];
+  quotes.push(quote)
+  sessionStorage.setItem('quotes',JSON.stringify(quotes));
+}
+
+function getQuotes(){
+    // Retrieving quotes from session storage
+    let quotes = JSON.parse(sessionStorage.getItem('quotes')) || [];
+    return quotes;
+}
+
+function addQuoteToForm(){
+    // Adds the quotes from session storage to the form
+    const quotes=getQuotes();
+    const parentDiv=document.getElementById('saved_quote');
+    quotes.forEach(quote=>{
+      parentDiv.value=quote;
+    })
+}
