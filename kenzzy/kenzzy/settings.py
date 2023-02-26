0b4f35c97fc454d3d340eed0df5169954b1ea2bc
Samuel Kabati <sam.kabati@gmail.com>
2023-02-26 12:04:04 +0300

    config

--- kenzzy/kenzzy/settings.py
+++ kenzzy/kenzzy/settings.py
@@ -25,7 +25,7 @@ STATICFILES_DIRS = [
 SECRET_KEY = "django-insecure-pqu3m)^by78m0qrit9s(q1+-k2w^zb$wd7b5ss$i)@mg2*4=o6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost','kenzzy-production.up.railway.app']
 
@@ -40,8 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     'whitenoise.runserver_nostatic',
-    'corsheaders',
     'kenzzyUI',
+    'corsheaders',
  
 ]
 
@@ -53,8 +53,8 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'corsheaders.middleware.CorsMiddleware',
     "whitenoise.middleware.WhiteNoiseMiddleware",
-    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = "kenzzy.urls"
